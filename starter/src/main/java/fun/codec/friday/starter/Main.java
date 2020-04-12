5db7057ae5dd689ea835912c232b26c92e6a6394
hanfei <echo@mail.sdufe.edu.cn>
2020-04-12 23:08:40 +0800

    jmx测试

---
+++ starter/src/main/java/fun/codec/friday/starter/Main.java
@@ -0,0 +1,20 @@
+package fun.codec.friday.starter;
+
+import javax.management.JMException;
+import javax.management.MBeanServer;
+import javax.management.remote.JMXConnectorServer;
+import javax.management.remote.JMXConnectorServerFactory;
+import javax.management.remote.JMXServiceURL;
+import java.lang.management.ManagementFactory;
+import java.rmi.registry.LocateRegistry;
+
+public class Main {
+    public static void main(String[] args) throws JMException, Exception {
+        MBeanServer server = ManagementFactory.getPlatformMBeanServer();
+        LocateRegistry.createRegistry(8081);
+        JMXServiceURL url = new JMXServiceURL
+                ("service:jmx:rmi:///jndi/rmi://localhost:8081/jmxrmi");
+        JMXConnectorServer jcs = JMXConnectorServerFactory.newJMXConnectorServer(url, null, server);
+        jcs.start();
+    }
+}
