2fdbb9f1fdb67c90469137a6fda850a0afac688a
zifeihan <caogui@iscas.ac.cn>
2022-05-14 11:42:43 +0800

    Add extractor for command version (#8)

---
+++ extractor/src/main/java/fun/codec/friday/extractor/ClassFileExtractor.java
@@ -0,0 +1,92 @@
+package fun.codec.friday.extractor;
+
+import com.sun.tools.attach.VirtualMachine;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
+import java.util.Arrays;
+import org.benf.cfr.reader.Main;
+
+public class ClassFileExtractor {
+
+    public static void agentmain(String cmdline, Instrumentation inst) throws Exception {
+
+        Class<?>[] classes = Arrays.stream(inst.getAllLoadedClasses())
+                                   .filter(c -> c.getName().startsWith(cmdline) && !c.isArray() && inst.isModifiableClass(c))
+                                   .toArray(Class[]::new);
+
+        ClassFileTransformer extractor = new ClassFileTransformer() {
+            @Override
+            public byte[] transform(ClassLoader loader, String className, Class<?> cls, ProtectionDomain pd, byte[] classBytes) {
+                if (null != classBytes) {
+                    try {
+                        File tmpFile = new File(SystemInfo.WORK_SPACE + File.separator + className.replace('.', '/') + ".class");
+                        if (!tmpFile.exists()) {
+                            String parent = tmpFile.getParent();
+                            File file = new File(parent);
+                            if (!file.exists()) {
+                                file.mkdirs();
+                            }
+                            tmpFile.createNewFile();
+                        }
+                        FileOutputStream fileOutputStream = new FileOutputStream(tmpFile);
+                        fileOutputStream.write(classBytes);
+                        fileOutputStream.flush();
+
+                        String clazzName = className.replaceAll("/", ".");
+                        decompile(clazzName);
+                        System.out.println(String.format("transform %s success", className));
+                    } catch (Throwable e) {
+                        System.out.println(String.format("transform %s error %s", className, e.getMessage()));
+                    }
+                }
+                return classBytes;
+            }
+        };
+
+        inst.addTransformer(extractor, true);
+        try {
+            inst.retransformClasses(classes);
+        } finally {
+            inst.removeTransformer(extractor);
+        }
+    }
+
+    private static void decompile(String className) {
+        try {
+            String clazzPath = SystemInfo.WORK_SPACE + File.separator + className.replace('.', '/') + ".class";
+            Main.main(new String[] {
+                clazzPath,
+                "--outputdir",
+                SystemInfo.WORK_SPACE
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String url = ClassFileExtractor.class.getProtectionDomain().getCodeSource().getLocation().toString();
+        if (!url.startsWith("file:/") || !url.endsWith(".jar")) {
+            System.out.println("Must be a JAR file");
+            System.exit(1);
+        }
+        String jar = url.substring(5);
+        if (args.length < 2) {
+            System.out.println("Usage: java -jar " + jar + " <pid> [prefix]");
+            System.exit(1);
+        }
+
+        VirtualMachine vm = VirtualMachine.attach(args[0]);
+        String cmdline = args.length > 2 ? args[1] + ' ' + args[2] : args[1];
+        try {
+            vm.loadAgent(jar, cmdline);
+        } finally {
+            vm.detach();
+        }
+
+        System.out.println("Done");
+    }
+}
---
+++ extractor/src/main/java/fun/codec/friday/extractor/SystemInfo.java
@@ -0,0 +1,38 @@
+package fun.codec.friday.extractor;
+
+import java.io.File;
+
+public class SystemInfo {
+    public static String HOME_PATH = System.getProperty("user.home") + File.separator + "friday";
+
+    public static String WORK_SPACE = HOME_PATH + File.separator + RuntimeMXBeanUtils.getPid();
+
+    public static String getTreePath(int pid) {
+        return HOME_PATH + File.separator + pid + File.separator + "dir";
+    }
+
+    public static String getClazzPath(int pid) {
+        return HOME_PATH + File.separator + pid + File.separator + "class";
+    }
+
+    public static String getDumpPath(int pid) {
+        return HOME_PATH + File.separator + pid + File.separator + "dump";
+    }
+
+    public static String getLogDir() {
+        File file = new File(SystemInfo.WORK_SPACE);
+        if (file.exists()) {
+            EFile.deleteFile(file);
+        } else {
+            file.mkdirs();
+        }
+
+        File logDir = new File(SystemInfo.WORK_SPACE + File.separator + "log");
+        if (logDir.exists()) {
+            EFile.deleteFile(logDir);
+        } else {
+            logDir.mkdirs();
+        }
+        return logDir.getAbsolutePath();
+    }
+}
